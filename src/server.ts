8c506431288dbc85e821bbac860da1c6527b47e0
JuliaJau <jautzj@gmail.com>
2021-08-10 15:25:03 +0200

    Delete entry and change to console.log

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,1 @@
-import { readFile } from 'fs/promises';
-
-async function readPasswords() {
-  const db = await readFile('./src/db.json', 'utf-8');
-  console.log(db);
-}
-readPasswords();
+console.log('Hello Server!');
