9b9fdd345de8df400c539a62cad6805034e1904e
JuliaJau <jautzj@gmail.com>
2021-08-10 16:36:34 +0200

    Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Server!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_request, response) => {
+  response.send('Hello Credentials!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
