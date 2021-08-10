3dc19dba904327b34a4b7c1bb29f917d4bd21555
JuliaJau <jautzj@gmail.com>
2021-08-10 18:24:08 +0200

    ✨ Add functionality to get route /api/credential

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import { readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (_request, response) => {
-  response.send('Hello Credentials!');
+app.get('/api/credentials/:search', async (req, res) => {
+  res.send(req.params.search);
+});
+
+app.get('/', (_req, res) => {
+  res.send('Hello Credentials');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
