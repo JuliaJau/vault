77b97c86850eed3a1c60c9de2dea2cf38a30e042
JuliaJau <jautzj@gmail.com>
2021-08-10 15:07:51 +0200

    :sparkles: Create readCredential function

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,9 +1,9 @@
 import { readFile } from 'fs/promises';
 import { DB, Credential } from '../types';
 
-export async function readCredentials(key: string): Promise<Credential> {
-  const dbData = await readFile('./src/db.json', 'utf-8');
-  const db: DB = JSON.parse(dbData);
+export async function readCredentials(): Promise<Credential[]> {
+  const response = await readFile('./src/db.json', 'utf-8');
+  const db: DB = JSON.parse(response);
   const credentials = db.credentials;
   return credentials;
 }
