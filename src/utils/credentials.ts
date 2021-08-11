ecad048c66ed0663f49c005a50dd9410f370b507
JuliaJau <jautzj@gmail.com>
2021-08-11 14:40:40 +0200

    :sparkles: Create function to add new Credential to DB

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,8 +1,9 @@
+import { writeFile } from 'fs';
 import { readFile } from 'fs/promises';
 import { DB, Credential } from '../types';
 
 export async function readCredentials(): Promise<Credential[]> {
-  const response = await readFile('./src/db.json', 'utf-8');
+  const response = await readFile('/src/db.json', 'utf-8');
   const db: DB = JSON.parse(response);
   const credentials = db.credentials;
   return credentials;
@@ -18,4 +19,22 @@ export async function getCredential(service: string): Promise<Credential> {
     throw new Error(`No credential found for service: $(service)`);
   }
   return credential;
+}
+
+export async function addCredential(credential: Credential): Promise<void> {
+  // waits for function above and saves in var
+  const credentials = await readCredentials();
+  // add an object to the credentials array
+  const newCredentials = [...credentials, credential];
+
+  const newDB: DB = {
+    credentials: newCredentials,
+  };
+
+  await writeFile('/src/db.json', JSON.stringify(newDB));
+
+  // add argument to excisting credentials
+  // create new DB
+  // overwrite DB using writeFile :tada:
+  // Bonus: extract function, add setDB, accepts DB objects overwrites DB
 }
