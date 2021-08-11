54609a177b8715f2cd484b5bed58124936f17fd8
JuliaJau <jautzj@gmail.com>
2021-08-11 12:03:33 +0200

    :sparkles: Create route to get single credential

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
 import express from 'express';
-import { readCredentials } from './utils/credentials';
+import { getCredential, readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
 
-app.get('/api/credentials/:search', async (req, res) => {
-  res.send(req.params.search);
+app.get('/api/credentials', async (_request, response) => {
+  try {
+    const credentials = await readCredentials();
+    response.status(200).json(credentials);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Internal Server Error! Please try again later.');
+  }
 });
 
-app.get('/', (_req, res) => {
-  res.send('Hello Credentials');
+app.get('/api/credentials/:service', async (request, response) => {
+  const { service } = request.params;
+  try {
+    const credential = await getCredential(service);
+    response.status(200).json(credential);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find service: ${service}`);
+  }
+});
+
+app.get('/', (_request, response) => {
+  response.send('Hello Credentials');
 });
 
 app.listen(port, () => {
