142913ae7cf053c9b5b700ef4689ab0781084924
JuliaJau <jautzj@gmail.com>
2021-08-12 16:46:46 +0200

    ➕ Add crypo.ts

---
+++ src/utils/crypto.ts
@@ -0,0 +1,28 @@
+import CryptoJS from 'crypto-js';
+import type { Credential } from '../types';
+
+export function encryptCredential(credential: Credential): Credential {
+  const encryptedPassword = CryptoJS.TripleDES.encrypt(
+    credential.password,
+    'currywurst'
+  ).toString();
+
+  const encryptedCredential = {
+    ...credential,
+    password: encryptedPassword,
+  };
+  return encryptedCredential;
+}
+
+export function decryptCredential(credential: Credential): Credential {
+  const decryptedPassword = CryptoJS.TripleDES.decrypt(
+    credential.password,
+    'currywurst'
+  ).toString(CryptoJS.enc.Utf8);
+
+  const decryptedCredential = {
+    ...credential,
+    password: decryptedPassword,
+  };
+  return decryptedCredential;
+}
